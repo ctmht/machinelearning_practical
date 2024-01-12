066f62c1a5b0404b5d7de9e350962501a7eb8695
Majo <m.bednar@student.rug.nl>
2024-01-12 14:38:19 +0100

    minor fix

--- src/api_request.py
+++ src/api_request.py
@@ -2,10 +2,10 @@ import requests
 import emoji_class_unicode_mapping as mp
 
 url = "http://127.0.0.1:8000/emoji_prediction/"
-payload = {"text": input("Enter a text: ")}
+request = {"text": input("Enter a text: ")}
 headers = {"Content-Type": "application/json"}
 
-response = requests.post(url, json=payload, headers=headers)
+response = requests.post(url, json=request, headers=headers)
 
 print(response.status_code)
 response_dict: dict = response.json()
