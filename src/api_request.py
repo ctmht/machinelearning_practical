51b2c1a5c3c6edd73a525c8fc98aeeeefb6a9fa8
Majo <m.bednar@student.rug.nl>
2024-01-12 14:32:30 +0100

    added python script that sends a request to api and prints a response (also added emoji unicode mapping for it)

---
+++ src/api_request.py
@@ -0,0 +1,14 @@
+import requests
+import emoji_class_unicode_mapping as mp
+
+url = "http://127.0.0.1:8000/emoji_prediction/"
+payload = {"text": input("Enter a text: ")}
+headers = {"Content-Type": "application/json"}
+
+response = requests.post(url, json=payload, headers=headers)
+
+print(response.status_code)
+response_dict: dict = response.json()
+
+for key, value in response_dict.items():
+    print(f"{key}: class {value} ( {mp.emoji_unicode[value]} )")
